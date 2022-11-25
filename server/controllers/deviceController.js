ad13b7b0a49b2094a5f167c3fb2e30c5db5dd778
Ivan Charot <ivancharot@gmail.com>
2022-11-25 12:26:46 +0300

    controllers done

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,18 @@
+class DeviceController {
+
+    create = async (req, res) => {
+
+    }
+
+    getAll = async (req, res) => {
+        
+    }
+
+    getOne = async (req, res) => {
+        
+    }
+
+}
+
+
+module.exports = new DeviceController()
---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+class UserController {
+
+    registration = async (req, res) => {
+
+    }
+
+    login = async (req, res) => {
+        
+    }
+
+    check = async (req, res) => {
+
+    }
+
+}
+
+
+module.exports = new UserController()
